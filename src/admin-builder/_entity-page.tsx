961843d43708439ff9a2062a7d7ee7bacd34ae9e
Gagik Antonyan <gagika57@gmail.com>
2024-06-06 21:44:18 +0500

    ✨ Add EntityCard component

--- src/admin-builder/_entity-page.tsx
+++ src/admin-builder/_entity-page.tsx
@@ -1,32 +1,31 @@
 import { Button } from '@/components/ui/button'
+import { EntityCardProvider } from './_entity-card'
 import { AdminClientEntityBuilderContainer } from './_container'
 
 export const EntityPageProvider = AdminClientEntityBuilderContainer.provider(
-	({ deps: { config, action } }) => {
+	({ deps: { config, action }, innerDeps: { EntityCard } }) => {
 		return function EntityPage() {
 			return (
-				<div className='p-5'>
-					<h1>{config.title}</h1>
-					<div className='flex gap-4'></div>
-					<Button
-						onClick={() => {
-							action()
-						}}
-					>
-						Create
-					</Button>
+				<div className='p-12 max-w-[1200px] mx-auto'>
+					<h1 className='text-5xl font-bold mb-8'>{config.title}</h1>
+					<div className='flex gap-4 mb-8'>
+						<Button
+							onClick={() => {
+								action()
+							}}
+						>
+							Create
+						</Button>
+					</div>
 
-					<div>
-						{config.fields.map((field) => {
-							return (
-								<div key={field.name}>
-									{field.type}: {field.name}
-								</div>
-							)
-						})}
+					<div className='grid grid-cols-3'>
+						<EntityCard />
 					</div>
 				</div>
 			)
 		}
+	},
+	{
+		EntityCard: EntityCardProvider,
 	}
 )
