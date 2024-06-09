9849128ed73a85ce277bda3a54bfa71d3c2fe19e
Gagik Antonyan <gagika57@gmail.com>
2024-06-09 17:23:47 +0500

    ⬆️ Upgrade entities types & action

--- src/admin-builder/_action.ts
+++ src/admin-builder/_action.ts
@@ -1,8 +1,41 @@
+import { eq } from 'drizzle-orm'
 import { AdminActionEntityBuilderContainer } from './_container'
+import { DbProvider } from './_db'
+import { EntitySchemaProvider } from './_entity-schema'
+import { ActionParams, AdminBuilderAction, GetEntitiesResult } from './_types'
 
 export const ActionProvider = AdminActionEntityBuilderContainer.provider(
-	ctx => async () => {
+	({innerDeps: { db, entitySchema }}): AdminBuilderAction => 
+		async (params: ActionParams): Promise<GetEntitiesResult[] | null> => {
+			if (params.type === 'create') {
+				await db.insert(entitySchema).values({
+					...params.data
+				})
+				return null
+			}
+			
+			if (params.type === 'get') {
+				return await db.query.entitySchema.findMany()
+			}
 
-		console.log('server component', ctx.deps.config)
+			if (params.type === 'update') {
+				await db
+					.update(entitySchema)
+					.set({ ...params.data })
+					.where(eq(entitySchema.id, params.id))
+			}
+
+			if (params.type === 'delete') {
+				await db
+					.delete(entitySchema)
+					.where(eq(entitySchema.id, params.id))
+					return null
+			}
+
+			return null
+		},
+	{
+		db: DbProvider,
+		entitySchema: EntitySchemaProvider
 	}
 )
--- src/admin-builder/_types.ts
+++ src/admin-builder/_types.ts
@@ -16,6 +16,47 @@ type SelectField = {
 	options: Options[]
 }
 
+export type CreateEntitiesParams = {
+	type: 'create'
+	data: {
+		[key: string]: string | number | boolean	
+	}
+}
+
+export type GetEntitiesParams = {
+	type: 'get'
+	[key: string]: string | number | boolean | null | Date
+}
+
+
+export type UpdateEntitiesParams = {
+	type: 'update'
+	id: number
+	data: {
+		[key: string]: string | number | boolean	
+	}
+}
+
+export type DeleteEntitiesParams = {
+	type: 'delete'
+	id: number
+}
+
+export type GetEntitiesResult = {
+	id: number
+	[key: string]: string | number | boolean
+}
+
+export type ActionParams =  
+	| CreateEntitiesParams 
+	| GetEntitiesParams 
+	| UpdateEntitiesParams 
+	| DeleteEntitiesParams
+
+export type AdminBuilderAction = (
+	params: ActionParams
+) => Promise<GetEntitiesResult[] | null>
+
 export type AdminBuilderField = TextField | SelectField
 
 export type AdminEntityConfig = {
